aaf51eeb14970e9ffc740d0358b490311177b1dd
Haganta Brema Bangun <hagantabangun@gmail.com>
2022-04-24 18:15:59 +0700

    basic POST and GET request

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
+app.use(express.json())
+
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+]
+
+// GET
 app.get('/', (req, res) => {
     res.send("Hello World!")
 })
 
-app.listen(3000, () => console.log("Listening to 3000"))
+app.get('/api/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) res.status(404).send("We cannot find the requested course.")
+    res.send(course)
+})
+
+// POST
+app.post('/api/courses', (req,res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course)
+    res.send(course)
+})
+
+app.listen(port, () => console.log(`Listening to ${port}...`))
